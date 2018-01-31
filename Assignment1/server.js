fc879e5d3044648a73585abfe38e873302620c61
Annie Norenberg <annienorenberg@annies-mbp.mobile.asu.edu>
2018-01-31 10:58:21 -0700

    1

--- Assignment1/server.js
+++ Assignment1/server.js
@@ -17,10 +17,25 @@ app.get("/add", function (req, res) {
     res.send(result.toString());
 });
 
-app.get("/sub", function (req, res) {
-    res.send(req.query);
-});
 
+app.get("/sub", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result = a - b;
+    res.send(result.toString());
+});
+app.get("/mult", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result = a * b;
+    res.send(result.toString());
+});
+app.get("/div", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result = a / b;
+    res.send(result.toString());
+});
 app.use(methodOverride());
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
